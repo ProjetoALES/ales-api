912115ec71681f7f593ed55644f86e0edbc6ba61
vpalmerini <victorpalmerini@gmail.com>
2019-12-03 21:11:30 -0200

    feat: allow authentication by email

--- login/models.py
+++ login/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email

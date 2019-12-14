b7e47832e4d6c84d6bfe0b57727b7c008e676a73
vpalmerini <victorpalmerini@gmail.com>
2019-12-14 18:48:34 -0200

    ref: set name as a pk for Semester model and add comments to api urls

--- semesters/models.py
+++ semesters/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Semester(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, primary_key=True)
     created = models.DateField(auto_now_add=True)
     start = models.DateField()
     end = models.DateField()

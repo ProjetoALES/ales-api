08c3e3e7714ca071e34387606b8b89c1ceb655bf
vpalmerini <victorpalmerini@gmail.com>
2019-12-21 10:15:24 -0200

    feat: create Student base serializer

--- enroll/serializers.py
+++ enroll/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Professor
+from .models import Professor, Student
 
 from django.contrib.auth.hashers import make_password
 
@@ -12,4 +12,15 @@ class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = ['id', 'name', 'email', 'phone', 'is_manager',
-                  'gender', 'course', 'enroll_year', 'age', 'is_active', 'password']
+                  'gender', 'course', 'enroll_year', 'birthday', 'is_active', 'password']
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'email', 'phone', 'gender', 'birthday', 'document',
+                  'authorization', 'address_code', 'locomotion', 'travel_time', 'lunch_at_school', 'password']

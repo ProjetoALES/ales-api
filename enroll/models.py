e5c295290fd8de44fb4171e19b19baf123eadc2b
vpalmerini <victorpalmerini@gmail.com>
2019-12-21 10:14:54 -0200

    feat: create Student model

--- enroll/models.py
+++ enroll/models.py
@@ -3,18 +3,34 @@ from login.models import User
 
 
 class Professor(User):
-    GENDERS = [
-        ('M', 'MALE'),
-        ('F', 'FEMALE')
-    ]
-    gender = models.CharField(max_length=1, choices=GENDERS)
     course = models.CharField(max_length=100)
     enroll_year = models.IntegerField()
-    age = models.IntegerField()
 
     def save(self, *args, **kwargs):
         self.is_professor = True
         super(Professor, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name + ' - ' + self.email
+
+
+class Student(User):
+    document = models.CharField(max_length=20)
+    authorization = models.BooleanField(default=False)
+    address_code = models.CharField(max_length=20)
+    LOCOMOTIONS = [
+        ('BUS', 'Bus'),
+        ('CAR', 'Car'),
+        ('FOOT', 'Foot'),
+        ('BIKE', 'Bike')
+    ]
+    locomotion = models.CharField(max_length=20, choices=LOCOMOTIONS)
+    travel_time = models.TimeField()
+    lunch_at_school = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.is_student = True
+        super(Student, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name + ' - ' + self.email
